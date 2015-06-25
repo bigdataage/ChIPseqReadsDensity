df82d2dbd23c56a89de68676c820390d85d8c245
Yong Peng <bigdataage@gmail.com>
2015-06-25 10:18:14 +0800

    Create 8-0-dpos.sh

---
+++ DANPOS/Example_2/8-0-dpos.sh
@@ -0,0 +1,44 @@
+
+danpos.py    dpos    5-otherFormats/H2B_CM_input.bed         --bg   5-otherFormats/H2B_CM_input.bed    --out 7-DANPOS/0-dpos/H2B_CM_input        --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2B_CM_input.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2B_CM_week0_BRep1.bed   --bg   5-otherFormats/H2B_CM_input.bed    --out 7-DANPOS/0-dpos/H2B_CM_week0_BRep1  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2B_CM_week0_BRep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2B_CM_week0_BRep2.bed   --bg   5-otherFormats/H2B_CM_input.bed    --out 7-DANPOS/0-dpos/H2B_CM_week0_BRep2  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2B_CM_week0_BRep2.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2B_CM_week4_BRep1.bed   --bg   5-otherFormats/H2B_CM_input.bed    --out 7-DANPOS/0-dpos/H2B_CM_week4_BRep1  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2B_CM_week4_BRep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2B_CM_week4_BRep2.bed   --bg   5-otherFormats/H2B_CM_input.bed    --out 7-DANPOS/0-dpos/H2B_CM_week4_BRep2  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2B_CM_week4_BRep2.log.txt 2>&1                               
+
+                              
+danpos.py    dpos    5-otherFormats/H2bGFP_day0_rep1.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_day0_rep1   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_day0_rep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_day0_rep2.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_day0_rep2   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_day0_rep2.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_day1_rep1.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_day1_rep1   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_day1_rep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_day1_rep2.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_day1_rep2   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_day1_rep2.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week1_rep1.bed   --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week1_rep1  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week1_rep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week1_rep2.bed   --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week1_rep2  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week1_rep2.log.txt 2>&1                               
+
+
+danpos.py    dpos    5-otherFormats/H2bGFP_week2_rep1.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week2_rep1   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week2_rep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week2_rep2.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week2_rep2   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week2_rep2.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week4_rep1.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week4_rep1   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week4_rep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week6_rep1.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week6_rep1   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week6_rep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week6_rep2.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week6_rep2   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week6_rep2.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week8_rep1.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week8_rep1   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week8_rep1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week8_rep2.bed    --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week8_rep2   --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week8_rep2.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H2bGFP_week12_rep1.bed   --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H2bGFP_week12_rep1  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H2bGFP_week12_rep1.log.txt 2>&1                               
+
+
+danpos.py    dpos    5-otherFormats/H3_Lane1.bed             --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H3_Lane1            --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H3_Lane1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/H3_Lane2.bed             --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/H3_Lane2            --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/H3_Lane2.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/input_H2bGFP_Lane1.bed   --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/input_H2bGFP_Lane1  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/input_H2bGFP_Lane1.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/input_H2bGFP_Lane2.bed   --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/input_H2bGFP_Lane2  --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/input_H2bGFP_Lane2.log.txt 2>&1                               
+danpos.py    dpos    5-otherFormats/input_all.bed            --bg   5-otherFormats/input_all.bed    --out 7-DANPOS/0-dpos/input_all           --height 3     --count 10000000    --span 20   --extend 100     --mifrsz 150   --mafrsz 400    >>7-DANPOS/0-dpos/input_all.log.txt 2>&1                               
+
+
+
+
+
+
+
+
+
+
+
+
+
