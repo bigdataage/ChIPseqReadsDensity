d4f24314a0ccba6d6e3ffd492d80593032c9bd93
Yong Peng <bigdataage@gmail.com>
2015-06-27 21:02:53 +0800

    Create 0-Homer.sh

---
+++ HOMER/0-Homer.sh
@@ -0,0 +1,13 @@
+# 1. install homer: http://homer.salk.edu/homer/index.html 
+
+# 2. Configuring HOMER
+cd   HOMER_dir
+perl  configureHomer.pl  -list
+
+perl  configureHomer.pl  -install  mm9
+perl  configureHomer.pl  -install  mouse-o
+perl  configureHomer.pl  -install  mouse-mRNA-3UTR
+perl  configureHomer.pl  -install  mouse-p
+perl  configureHomer.pl  -install  mouse-mRNA
+
+
